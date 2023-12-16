34680f2c8c65f22132e491fcf37cad59201671b1
Brenty_Boy <brenthamsley@gmail.com>
2023-12-16 10:25:43 -0800

    Main

---
+++ my_beautiful_code.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * print_code - Prints "my_beautiful_code"
+ */
+void print_code(void)
+{
+	printf("my_beautiful_code\n");
+}
+
+/**
+ * main - The main function.
+ * return - The exit function.
+ *
+ * Main: The starting point of the program.
+ * Return: 0 when exiting program.
+ */
+int main(void)
+{
+	print_code();
+	return (0);
+}
